45465608ec06f58f351feca5c75970d9af806ccb
Govind Krishna Mishra <professionalgkm@gmail.com>
2024-04-05 17:52:53 +0530

    database connect

--- api/index.js
+++ api/index.js
@@ -1,7 +1,28 @@
 import express from 'express'
-const PORT = 3000
+const PORT = 3001
 const app = express();
+import mongoose from 'mongoose'
+import 'dotenv/config'
 
-app.listen(3000,()=> {
-     `server is listen on ${PORT}`
+app.use(express.json());
+ 
+mongoose.connect(process.env.DB_LOCATION, {
+     autoIndex:true
 })
+.then(() => {
+    console.log('MongoDB is connected');
+    
+})
+.catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
+
+
+ 
+
+ 
